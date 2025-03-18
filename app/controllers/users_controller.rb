1ff23c942a9a2658fc1bd01d61349a6c89fab6e1
Md Ali Zulayed <ali.zulayed@kellogg.northwestern.edu>
2025-03-18 02:54:24 +0000

    Updated encrypted login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
+    @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
     redirect_to "/"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
   resources "entries"
   resources "places"
-  resources "sessions"
   resources "users"
+  resources "sessions"
+  
+  get("/login", {:controller => "sessions", :action => "new"})
+  get("/logout", {:controller => "sessions", :action => "destroy"})
+  
+  # Landing page (aka root route)
+  get("/", {:controller => "sessions", :action => "new"})  
+  
 end
